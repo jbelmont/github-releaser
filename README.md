56d7671db5359834664f652f8f2ae4156c7d30df
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-08-20 15:34:16 -0400

    Add deployment cli script and README with explanation on how to run.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# github-releaser
-A Golang CLI Script that creates a Github Release
+# Github Releaser
+
+A Golang CLI Script that creates a Github Release from the Github API
+
+```bash
+POST /repos/:owner/:repo/releases
+```
+
+## Create a Github Release Documentation
+
+[Github Releaser](https://developer.github.com/v3/repos/releases/#create-a-release)
+
+#### Request Body
+
+```json
+{
+  "tag_name": "v1.0.0",
+  "target_commitish": "master",
+  "name": "v1.0.0",
+  "body": "Description of the release",
+  "draft": false,
+  "prerelease": false
+}
+```
+
+#### Request Headers
+
+| Request Header | Example |
+| --- | --- |
+| `Content-Type` | `Content-Type: "application/json"` |
+| `Authorization` | `Authorization: "token someToken12345"` |
+
+## Example CLI run to create a release
+
+If you install this script and put your `GOPATH` in your `PATH` then you should be able to run the command like this:
+
+```bash
+github-releaser -tagName="v1.0.0" -targetCommitish=someCommit123445 -name="Your Release Name" -body="The body message" -username=jbelmont -repo=github-releaser
+```
+
+Else you will clone this repository and run the command like this:
+
+```bash
+go run main.go -tagName="v1.0.0" -targetCommitish=someCommit123445 -name="Your Release Name" -body="The body message" -username=jbelmont -repo=github-releaser
+```
---
+++ deploy.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	tagName         = flag.String("tagName", "", "Please provide a tag name for the release")
+	targetCommitish = flag.String("targetCommitish", "", "Please provide a targetCommitish value namely master")
+	name            = flag.String("name", "", "A name for the Github Release")
+	body            = flag.String("body", "", "Provide a description of the release")
+	username        = flag.String("username", "", "Enter your github username/organization")
+	repo            = flag.String("repo", "", "Enter your github repository")
+)
+
+func checkArgs() {
+	if *tagName == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *targetCommitish == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *name == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *body == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *username == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *repo == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	checkArgs()
+
+	body := struct {
+		TagName         string `json:"tag_name"`
+		TargetCommitish string `json:"target_commitish"`
+		Name            string `json:"name"`
+		Body            string `json:"body"`
+		Draft           bool   `json:"draft"`
+		Prerelease      bool   `json:"prerelease"`
+	}{
+		*tagName,
+		*targetCommitish,
+		*name,
+		*body,
+		false,
+		false,
+	}
+
+	postBody, err := json.Marshal(body)
+	if err != nil {
+		log.Fatalln("An issue occurred marshalling this data ", err)
+	}
+
+	client := http.Client{}
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.github.com/repos/"+*username+"/"+*repo+"/releases",
+		bytes.NewBuffer(postBody),
+	)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "token "+os.Getenv("PACKTCI_PERSONAL_TOKEN"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if resp.StatusCode != http.StatusCreated {
+		log.Fatalln("Should receive a status code of 201 created")
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["url"])
+}
